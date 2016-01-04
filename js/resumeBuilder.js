240637a4555eb58728c768cfb390c67d76217fa5
Jose24San <Sanchez24jose@gmail.com>
2016-01-03 21:37:38 -0600

    Added education information and changed the welcome message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,16 @@
 var bio = {
-    name: "Jose Sanchez ",
-    role: " Front End Web Developer",
-    welcomeMessage: "Hello I hope you didnt find this page by accident and that you were purposely looking for me. I am an a Biomedical engineer / Technologist who also happens to do a lot of programming to learn how build the front end of web pages. I've recently graduated from DeVry University in Irving, Texas. I am passionate about learning how to create what I believe is beautiful, meaningful and most importantly impactful. Most of my time is spent acquiring knowledge to be better in my job and in my relationships. The little time I do not spend growing I spend in the GYM pumping iron and anything that I can do to be active. A healthy life style is important to me so I also happen to love finding ways to cook the most delicious food possible that is as healthy as possible. I strive to live the good life.",
+    "name": "Jose Sanchez ",
+    "role": " Front End Web Developer",
+    "welcomeMessage": "Hello my name is Jose and I am a Biomedical Engineering Technologist who also happens to do a lot of programming to learn how build the front end of web pages. I've recently graduated from DeVry University in Irving, Texas. I am passionate about learning how to create what I believe is beautiful, meaningful and most importantly impactful. Most of my time is spent acquiring knowledge to be better in my job and in my relationships. The little time I do not spend expanding my knowledge base I spend in the GYM pumping iron and anything that I can do to be active. A healthy life style is important to me so I also love finding ways to cook delicious but healthy food. Giving back is also a priority for me and I do so through volunteer work in the Mentor 2.0 organization as a mentor for a highschool kid that attends a low income school. I strive to live the good life.",
 
-    image: "images/me.jpg",
-    contacts: [{
-        mobile: "928-846-1010",
-        skype: "Jose:)",
-        location: "Dallas, TX",
-        email: "Sanchez24Jose"
+    "image": "img/me.JPG",
+    "contacts": [{
+        "mobile": "928-846-1010",
+        "GitHub": "Jose24San",
+        "location": "Dallas, TX",
+        "email": "Sanchez24Jose@gmail.com"
     }],
-    skills: ["Character", "Patience", "Honesty", "Work Ethic"]
+    "skills": ["HTML", "CSS", "JavaScript", "jQuery", "Version Control"]
 };
 
 var work = {
@@ -19,7 +19,7 @@ var work = {
         "title": "Biomedical Technical Manager",
         "dates": "2015 - current",
         "location": "Dallas, TX",
-        "description": " Quality control and all biomedical equipment sold by my company. I also perform troubleshooting and repair on any peice of equipment that Vonco Medical sells. That includes on site repair for therpay clinics and rehabilitaion hospitals that do not have a Biomed department in house."
+        "description": " Quality control on all biomedical equipment sold by my company. I also perform troubleshooting and repair on any peice of equipment that Vonco Medical sells. That includes on site repair for therapy clinics and rehabilitaion hospitals that do not have a Biomed department in house."
     }, {
         "employer": "DeVry University",
         "title": "Federal Work Study Student",
@@ -31,15 +31,10 @@ var work = {
 
 var projects = {
     "project": [{
-        "title": "About me ",
-        "dates": "2015",
-        "description": "First project that I did in the Front end Nanodegree course that shares a little information about me.",
-        "images": ["images/udacity.jpe"]
-    }, {
         "title": "Project portfolio",
         "dates": "2015",
-        "description": "The second project of the Front end Nanodegree entailed creating a resonsive portfolio to house all future projects.",
-        "images": ["images/1.jpg", "images/3.jpg", "images/2.jpg"]
+        "description": "The second project of the Front end Nanodegree entailed creating a responsive portfolio to house all future projects. This portfolio can be found at Josedevport.com , the source codes can be viewed on my GitHub account under my username Jose24San. ",
+        
     }]
 };
 
@@ -51,13 +46,29 @@ var education = {
         "majors": " Biomedical Engineering Technology",
         "dates": "2011 - 2015",
         "url": "DeVry.edu"
-    }],
-    "online courses": [{
-        "title": "Front-End Web Developer Nanodegree",
-        "school": "udacity",
+    }, {
+        "name": "Udacity",
+        "location": "Online",
+        "degree": "Front-End Web Developer Nanodegree (in progress)",
+        "majors": "Front End Development",
         "dates": " 2015",
         "url": "https://www.udacity.com/course/nd001"
-    }, ]
+    }, {
+        "name": "Team Tree House",
+        "location": "Online",
+        "degree": "Front-End Web Development Track (in progress)",
+        "majors": "Front End Development",
+        "dates": " 2015",
+        "url": "https://www.udacity.com/course/nd001"
+    
+    }, {
+        "name": "Exercism.io",
+        "location": "Online",
+        "degree": "JavaScript Track (in progress)",
+        "majors": "JavaScript",
+        "dates": " 2015",
+        "url": "https://www.udacity.com/course/nd001"
+    }]
 };
 
 
@@ -78,8 +89,8 @@ bio.display = function() {
     for (contact in bio.contacts) {
         var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[contact].mobile);
         var formattedEmail = HTMLemail.replace("%data%", bio.contacts[contact].email);
-        var formattedSkype = HTMLcontactGeneric.replace("%contact%", "skype").replace("%data%", bio.contacts[contact].skype);
-        $("#footerContacts").append(formattedMobile, formattedEmail, formattedSkype);
+        var formattedGithub = HTMLcontactGeneric.replace("%contact%", "GitHub").replace("%data%", bio.contacts[contact].GitHub);
+        $("#footerContacts").append(formattedMobile, formattedEmail, formattedGithub);
     };
 };
 
